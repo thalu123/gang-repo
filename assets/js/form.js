82d28c5e18dce78c8d8501584af7dfd17b5c098d
Thalu123 <thalhathariyil@gmail.com>
2025-03-18 13:21:09 +0530

    Form working sucessfully

--- assets/js/form.js
+++
@@ -1,8 +0,0 @@
-function sentMail(){
-    let parms = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        message : document.getElementById("message").value,
-    }
-    emailjs.sent("service_nevy0jp","template_8d24ug5",parms).then(alert("mailsent!!"))
-}
